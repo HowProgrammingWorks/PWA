1ff27c02824f492a4f3b03be480e96432ce91fac
Timur Shemsedinov <timur.shemsedinov@gmail.com>
2025-07-29 19:04:34 +0300

    Add eslint and prettier

---
+++ eslint.config.js
@@ -0,0 +1,18 @@
+'use strict';
+
+const init = require('eslint-config-metarhia');
+
+module.exports = init;
+module.exports = [
+  ...init,
+  {
+    files: ['Application/**/*.js'],
+    languageOptions: {
+      sourceType: 'module',
+      globals: {
+        indexedDB: true,
+        prompt: true,
+      },
+    },
+  },
+];
